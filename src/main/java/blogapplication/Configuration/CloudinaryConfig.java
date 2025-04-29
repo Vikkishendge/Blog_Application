1eb3243fe1d3d04bfacc336df2d3b56721ff145e
Vikkishendge <shendvikas02@gmail.com>
2025-04-29 22:55:40 +0300

    change

--- src/main/java/blogapplication/Configuration/CloudinaryConfig.java
+++ src/main/java/blogapplication/Configuration/CloudinaryConfig.java
@@ -3,6 +3,7 @@ package blogapplication.Configuration;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,13 +12,21 @@ import com.cloudinary.Cloudinary;
 @Configuration
 public class CloudinaryConfig {
 	
+	@Value("${cloudinary.cloud_name}")
+	private String cloud_name;
+	
+	@Value("${cloudinary.api_key}")
+	private String api_key;
+	
+	@Value("${cloudinary.api_secret}")
+	private String api_secret;
 	@Bean
 	public Cloudinary cloudinary() {
 		Map<String,String> config=new HashMap<>();
 		
-		config.put("cloud_name","blogapplicatin");
-		config.put("api_key","724345369346844");
-		config.put("api_secret","A2BcD-slrqd2RYLTc5J9rFrRNwk");
+		config.put("cloud_name",cloud_name);
+		config.put("api_key",api_key);
+		config.put("api_secret",api_secret);
 		
 		return new Cloudinary(config);
 	}
